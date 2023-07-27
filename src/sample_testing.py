87058f3e8fc5c46357ff6158ab693a3fe7a24948
shashikiran745 <shashikiranburugala@gmail.com>
2023-07-27 16:23:18 +0530

    Second commit it seems :)

--- src/sample_testing.py
+++ src/sample_testing.py
@@ -2,3 +2,16 @@
 
 print("shashi")
 print("This is just for testing my git skills")
+
+
+# Done with testing the git just trying all the features
+
+class NormalClass:
+
+    def normal(self):
+        return "HELLO FOLKS"
+    
+    if __name__ == '__main__':
+        normal()
+    
+
